27065a5870122587dad0a1e3e6d9696b2eb166ae
Avinav Kumar <avinavkumar0045@gmail.com>
2026-06-22 11:39:27 +0530

    Initial commit

---
+++ sites/localsite.com/public/files/page_scripts/commit-reveal-b885d-09c8cc13ff.js
@@ -0,0 +1,2 @@
+(()=>{document.documentElement.classList.add("js-reveal");const els=document.querySelectorAll("[data-reveal]");if(!("IntersectionObserver"in window)){els.forEach((el)=>el.classList.add("in-view"));return;}
+const io=new IntersectionObserver((entries)=>{entries.forEach((entry)=>{if(entry.isIntersecting){entry.target.classList.add("in-view");io.unobserve(entry.target);}});},{rootMargin:"0px 0px -10% 0px"});els.forEach((el)=>io.observe(el));})();
